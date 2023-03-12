4e37dad6063b256a48bf22e8c9757986e97915ae
Marcio Curvello <marciocurvello@gmail.com>
2023-03-12 20:05:02 -0300

    feat: add personal info screen

---
+++ components/PersonalInfo.tsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import { Button, Image, Text, TextInput, View } from "react-native";
+import Styles from "./Styles";
+
+const PersonalInfo = () => {
+  const [name, setName] = useState(";");
+  return (
+    <View style={Styles.personalInfoContainer}>
+      <Image
+        style={Styles.logo}
+        source={require("../assets/wired-brain-coffee-logo.png")}
+      />
+      <View style={Styles.enterYourName}>
+        <Text style={Styles.nameText}>Please enter your name</Text>
+        <TextInput
+          style={Styles.nameTextInput}
+          onChangeText={(text) => setName(text)}
+          value={name}
+        />
+      </View>
+      <Button title="Start chatting!" onPress={() => {}} />
+    </View>
+  );
+};
+
+export default PersonalInfo;
