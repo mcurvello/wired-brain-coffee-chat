81427b1068fce144c9822159cc1d24a0d7dbb0f5
Marcio Curvello <marciocurvello@gmail.com>
2023-03-12 20:28:20 -0300

    feat: add imagechooser component

--- components/PersonalInfo.tsx
+++ components/PersonalInfo.tsx
@@ -1,9 +1,12 @@
 import React, { useState } from "react";
 import { Button, Image, Text, TextInput, View } from "react-native";
+import ImageChooser from "./ImageChooser";
 import Styles from "./Styles";
 
 const PersonalInfo = () => {
-  const [name, setName] = useState(";");
+  const [name, setName] = useState("");
+  const [image, setImage] = useState("");
+
   return (
     <View style={Styles.personalInfoContainer}>
       <Image
@@ -18,6 +21,7 @@ const PersonalInfo = () => {
           value={name}
         />
       </View>
+      <ImageChooser onChangeImage={(image) => setImage(image)} />
       <Button title="Start chatting!" onPress={() => {}} />
     </View>
   );
